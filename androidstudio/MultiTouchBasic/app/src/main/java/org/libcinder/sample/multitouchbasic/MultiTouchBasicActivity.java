311175b6db0960b2a2847b4b17ee8047abc39f12
mark <mark.korput@gmail.com>
2018-11-05 14:26:48 +0100

    re-generated xcode project from cmake and removed out-of-date androind and windows project files

--- androidstudio/MultiTouchBasic/app/src/main/java/org/libcinder/sample/multitouchbasic/MultiTouchBasicActivity.java
+++
@@ -1,7 +0,0 @@
-package org.libcinder.sample.multitouchbasic;
-
-import org.libcinder.app.CinderNativeActivity;
-
-public class MultiTouchBasicActivity extends CinderNativeActivity {
-    static final String TAG = "MultiTouchBasicActivity";
-}
